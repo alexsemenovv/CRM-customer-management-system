160c088453e2360c98f0e0ae8c56ae9db1849beb
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-05 12:33:37 +0500

    add path for login

---
+++ DjangoCRM/myauth/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+app_name = "myauth"
+
+urlpatterns = [
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="myauth/login.html",
+            redirect_authenticated_user=True,
+            # эта опция нужна для того чтобы аутентифицированному пользователю не нужно было снова вводить логин и пароль
+        ),
+        name="login"
+    ),
+]
