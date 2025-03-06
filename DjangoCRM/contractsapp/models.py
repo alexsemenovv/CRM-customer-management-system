753ab12d18ec20dbccefbd747d149246fcb13a85
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-06 18:40:21 +0500

    create model Contract

--- DjangoCRM/contractsapp/models.py
+++ DjangoCRM/contractsapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from productapp.models import Product
+
+
+class Contract(models.Model):
+    """Сущность: контракт для клиента"""
+    title = models.CharField(max_length=200, verbose_name="Название контракта")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Предоставляемая услуга')
+    document = models.FileField(null=True, upload_to='documents/contract/', verbose_name="Файл с документом")
+    start_date = models.DateField(auto_now_add=True, verbose_name="Дата заключения")
+    end_date = models.DateField(verbose_name="Период действия")
+    cost = models.DecimalField(default=0, max_digits=8, decimal_places=2, verbose_name='Сумма')
