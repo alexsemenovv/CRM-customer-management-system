c5d02d9f39f04494f5b16dd38f4ec38b5a7c3e44
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-06 17:03:17 +0500

    added model Lead

--- DjangoCRM/leadsapp/models.py
+++ DjangoCRM/leadsapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from adsapp.models import Ad
+
+
+class Lead(models.Model):
+    """Сущность: потенциальный клиент"""
+    full_name = models.CharField(max_length=255, verbose_name="Ф.И.О.")
+    phone_number = models.CharField(max_length=12, verbose_name="Телефон")
+    email = models.EmailField(unique=True, verbose_name="Email")
+    ad = models.OneToOneField(Ad, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.full_name
+
