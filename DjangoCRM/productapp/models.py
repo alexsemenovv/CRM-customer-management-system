ad4255922d6207639515d2491c056955f7e296fc
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-06 11:18:25 +0500

    added model Product

--- DjangoCRM/productapp/models.py
+++ DjangoCRM/productapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"Услуга: {self.name}\nОписание: {self.description}\nСтоимость: {self.price}"
