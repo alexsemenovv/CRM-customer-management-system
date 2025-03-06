6f9374bb191ce68269277276bb6c543e365302cd
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-06 13:56:24 +0500

    added AdsCreateView

--- DjangoCRM/adsapp/urls.py
+++ DjangoCRM/adsapp/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from .views import (
     AdsListView,
+    AdsCreateView,
 )
+
+app_name = 'adsapp'
 
 urlpatterns = [
     path('', AdsListView.as_view(), name='ads_list'),
+    path('new/', AdsCreateView.as_view(), name='ads_create'),
 ]
--- DjangoCRM/adsapp/views.py
+++ DjangoCRM/adsapp/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy, reverse
 from django.views.generic import (
     ListView,
+    CreateView,
 )
 
 from .models import Ad
@@ -10,5 +11,13 @@ class AdsListView(ListView):
     """Список всех рекламных компаний"""
     template_name = "adsapp/ads_list.html"
     context_object_name = 'ads'
-    queryset = Ad.objects.prefetch_related('Product').all()
+    queryset = Ad.objects.prefetch_related('product').all()
 
+
+class AdsCreateView(CreateView):
+    """Создание рекламной компании"""
+    template_name = "adsapp/ads_create.html"
+    model = Ad
+    fields = "name", "product", "promotion_channel", "advertising_budget"
+
+    success_url = reverse_lazy("adsapp:ads_list")
