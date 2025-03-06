74196efc56b14cc19529355361dd595d17cdeb54
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-06 13:31:57 +0500

    added model Ad, and AddApp in settings

--- DjangoCRM/DjangoCRM/settings.py
+++ DjangoCRM/DjangoCRM/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'myauth.apps.MyauthConfig',
     'productapp.apps.ProductappConfig',
+    'adsapp.apps.AdsappConfig',
 ]
 
 MIDDLEWARE = [
--- DjangoCRM/adsapp/models.py
+++ DjangoCRM/adsapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from productapp.models import Product
+
+
+class Ad(models.Model):
+    """Сущность: рекламная компания"""
+    name = models.CharField(max_length=100)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)  # услуга
+    promotion_channel = models.CharField(max_length=100)  # канал продвижения
+    advertising_budget = models.DecimalField(default=0, max_digits=8, decimal_places=2)
