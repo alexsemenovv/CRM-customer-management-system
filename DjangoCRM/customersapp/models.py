95f4a30e957f152d7f77d592934d82cec0e54d45
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-07 06:25:18 +0500

    added model Customer

--- DjangoCRM/customersapp/models.py
+++ DjangoCRM/customersapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from contractsapp.models import Contract
+from leadsapp.models import Lead
+
+
+class Customer(models.Model):
+    """Сущность: активный клиент"""
+    lead = models.OneToOneField(Lead, on_delete=models.CASCADE)
+    contract = models.OneToOneField(Contract, on_delete=models.CASCADE)
