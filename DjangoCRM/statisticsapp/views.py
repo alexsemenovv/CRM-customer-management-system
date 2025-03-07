a96424a8acb5baff444d0cda7210391590bc15ad
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-07 14:39:57 +0500

    added company_succes

--- DjangoCRM/statisticsapp/views.py
+++ DjangoCRM/statisticsapp/views.py
@@ -1,6 +1,6 @@
 from http.client import HTTPResponse
 
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.http import HttpRequest
 from django.shortcuts import render
 
@@ -12,19 +12,32 @@ from productapp.models import Product
 
 
 def get_statistics(request: HttpRequest) -> HTTPResponse:
-    """Функция считает общую статистику"""
+    """
+    Функция считает общую статистику:
+    - Кол-во услуг
+    - Кол-во рекламных компаний, а также кол-во лидов для каждой
+    - Успешность каждой компании в %
+    - Общее кол-во лидов
+    - Общее кол-во активных клиентов
+    """
     products = Product.objects.all()  # услуги
     leads = Lead.objects.annotate(count=Count("id"))  # потенциальные клиенты
     customers = Customer.objects.annotate(count=Count("id"))  # активные клиенты
+
     ads = Lead.objects.raw(
         """
-        select aa.id, aa.name, COUNT(ll.id) as count
-        from leadsapp_lead ll 
-        JOIN adsapp_ad aa on aa.id = ll.ad_id
+        SELECT 
+            aa.id,
+            aa.name,
+            COUNT(ll.id) as count_leads,
+            (SUM(cc.cost) - aa.advertising_budget) / 100 as company_success
+        FROM contractsapp_contract cc 
+        JOIN productapp_product pp on pp.id = cc.product_id
+        JOIN adsapp_ad aa on aa.product_id  = pp.id
+        JOIN leadsapp_lead ll on ll.ad_id = aa.id
         GROUP BY aa.id
         """
     )
-    print(customers)
 
     context = {
         "products": products,
@@ -33,7 +46,3 @@ def get_statistics(request: HttpRequest) -> HTTPResponse:
         "customers": customers,
     }
     return render(request, "statisticsapp/index.html", context=context)
-
-# TODO
-#  число клиентов, перешедших из потенциальных в активных;
-#  соотношение дохода от контрактов и расходов на рекламу.
