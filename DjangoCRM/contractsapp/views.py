a06f94dcfb83d3dc0d7a5342f85815b5a815ed3d
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-08 12:27:53 +0500

    added permission required for all views

--- DjangoCRM/contractsapp/views.py
+++ DjangoCRM/contractsapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy, reverse
 from django.views.generic import (
     CreateView,
@@ -8,29 +9,33 @@ from .models import Contract
 from .forms import ContractForm
 
 
-class ContractListView(ListView):
+class ContractListView(PermissionRequiredMixin, ListView):
     """Просмотр всех контрактов"""
+    permission_required = "contractsapp.view_contract"
     template_name = "contractsapp/contracts_list.html"
     context_object_name = 'contracts'
     queryset = Contract.objects.prefetch_related('product').all()
 
 
-class ContractCreateView(CreateView):
+class ContractCreateView(PermissionRequiredMixin, CreateView):
     """Создание контракта"""
+    permission_required = "contractsapp.add_contract"
     template_name = "contractsapp/contracts_create.html"
     model = Contract
     form_class = ContractForm
     success_url = reverse_lazy("contractsapp:contracts_list")
 
 
-class ContractDetailsView(DetailView):
+class ContractDetailsView(PermissionRequiredMixin, DetailView):
     """Просмотр деталей контракта"""
+    permission_required = "contractsapp.view_contract"
     template_name = "contractsapp/contracts_detail.html"
     model = Contract
 
 
-class ContractUpdateView(UpdateView):
+class ContractUpdateView(PermissionRequiredMixin, UpdateView):
     """Редактирование контракта"""
+    permission_required = "contractsapp.change_contract"
     template_name = "contractsapp/contracts_update.html"
     model = Contract
     form_class = ContractForm
@@ -46,8 +51,9 @@ class ContractUpdateView(UpdateView):
         )
 
 
-class ContractDeleteView(DeleteView):
+class ContractDeleteView(PermissionRequiredMixin, DeleteView):
     """Удаление контракта"""
+    permission_required = "contractsapp.delete_contract"
     template_name = "contractsapp/contracts_delete.html"
     model = Contract
     success_url = reverse_lazy("contractsapp:contracts_list")
