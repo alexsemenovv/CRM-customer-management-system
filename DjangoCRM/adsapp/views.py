762eee30ef3d37bd01f6ad0fba09fcbd63c437b9
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-08 12:24:04 +0500

    added permission required for all views

--- DjangoCRM/adsapp/views.py
+++ DjangoCRM/adsapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy, reverse
 from django.views.generic import (
     ListView,
@@ -8,15 +9,17 @@ from django.views.generic import (
 from .models import Ad
 
 
-class AdsListView(ListView):
+class AdsListView(PermissionRequiredMixin, ListView):
     """Список всех рекламных компаний"""
+    permission_required = "adsapp.view_ad"
     template_name = "adsapp/ads_list.html"
     context_object_name = 'ads'
     queryset = Ad.objects.prefetch_related('product').all()
 
 
-class AdsCreateView(CreateView):
+class AdsCreateView(PermissionRequiredMixin, CreateView):
     """Создание рекламной компании"""
+    permission_required = "adsapp.add_ad"
     template_name = "adsapp/ads_create.html"
     model = Ad
     fields = "name", "product", "promotion_channel", "advertising_budget"
@@ -24,14 +27,16 @@ class AdsCreateView(CreateView):
     success_url = reverse_lazy("adsapp:ads_list")
 
 
-class AdsDetailView(DetailView):
+class AdsDetailView(PermissionRequiredMixin, DetailView):
     """Просмотр деталей компании"""
+    permission_required = "adsapp.view_ad"
     template_name = "adsapp/ads_detail.html"
     model = Ad
 
 
-class AdsUpdateView(UpdateView):
+class AdsUpdateView(PermissionRequiredMixin, UpdateView):
     """Редактирование компании"""
+    permission_required = "adsapp.change_ad"
     template_name = "adsapp/ads_update.html"
     model = Ad
     fields = "name", "product", "promotion_channel", "advertising_budget"
@@ -46,8 +51,9 @@ class AdsUpdateView(UpdateView):
             kwargs={"pk": self.object.pk},
         )
 
-class AdsDeleteView(DeleteView):
+class AdsDeleteView(PermissionRequiredMixin, DeleteView):
     """Удаление рекламной компании"""
+    permission_required = "adsapp.delete_ad"
     template_name = "adsapp/ads_delete.html"
     model = Ad
     success_url = reverse_lazy("adsapp:ads_list")
