61b193d3f8db9ced1048e63e5a067d7a3a26faf6
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-08 07:13:13 +0500

    added method __str__ for model Customer

--- DjangoCRM/customersapp/models.py
+++ DjangoCRM/customersapp/models.py
@@ -8,3 +8,6 @@ class Customer(models.Model):
     """Сущность: активный клиент"""
     lead = models.OneToOneField(Lead, on_delete=models.CASCADE, verbose_name="Потенциальный клиент")
     contract = models.OneToOneField(Contract, on_delete=models.CASCADE, verbose_name="Контракт")
+
+    def __str__(self):
+        return self.lead
