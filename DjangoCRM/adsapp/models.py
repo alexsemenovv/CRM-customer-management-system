2692889b8415406d08f7d112b85291dda76007da
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-08 07:08:02 +0500

    added verbose name for model Ad

--- DjangoCRM/adsapp/models.py
+++ DjangoCRM/adsapp/models.py
@@ -5,10 +5,10 @@ from productapp.models import Product
 
 class Ad(models.Model):
     """Сущность: рекламная компания"""
-    name = models.CharField(max_length=100)
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)  # услуга
-    promotion_channel = models.CharField(max_length=100)  # канал продвижения
-    advertising_budget = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    name = models.CharField(max_length=100, verbose_name="Название рекламной компании")
+    product = models.OneToOneField(Product, on_delete=models.CASCADE, verbose_name="Услуга")
+    promotion_channel = models.CharField(max_length=100, verbose_name="Канал продвижения")
+    advertising_budget = models.DecimalField(default=0, max_digits=8, decimal_places=2, verbose_name="Бюджет на рекламу")
 
     def __str__(self):
         return f"{self.name}"
