8f02b27b75359fdfe78ca413d93f67bd44bb7859
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-08 07:11:10 +0500

    added verbose name for model Product

--- DjangoCRM/productapp/models.py
+++ DjangoCRM/productapp/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=False, blank=True)
-    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    name = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(null=False, blank=True, verbose_name="Описание")
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2, verbose_name="Стоимость")
 
     def __str__(self):
         return f"{self.name}"
