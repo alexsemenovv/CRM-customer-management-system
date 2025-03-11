5392c35664f62fc85886f99618ffdbb289725e57
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-11 08:52:35 +0500

    configurate for postgresql; added fixtures

--- DjangoCRM/DjangoCRM/settings.py
+++ DjangoCRM/DjangoCRM/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'DjangoCRM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
