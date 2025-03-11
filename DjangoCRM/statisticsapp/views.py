e4b9e6cc85baa762b04dd4cfbd23731c0a9fad9f
Aleksandr <aleksandrsemeonow@yandex.ru>
2025-03-11 07:45:09 +0500

    added redirect for statistics

--- DjangoCRM/statisticsapp/views.py
+++ DjangoCRM/statisticsapp/views.py
@@ -2,7 +2,7 @@ from http.client import HTTPResponse
 
 from django.db.models import Count, Sum, ExpressionWrapper, F, DecimalField
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from adsapp.models import Ad
 from customersapp.models import Customer
@@ -19,22 +19,25 @@ def get_statistics(request: HttpRequest) -> HTTPResponse:
     - Общее кол-во лидов
     - Общее кол-во активных клиентов
     """
-    products = Product.objects.all()  # услуги
-    leads = Lead.objects.annotate(count=Count("id"))  # потенциальные клиенты
-    customers = Customer.objects.annotate(count=Count("id"))  # активные клиенты
+    if request.user.is_authenticated:
+        products = Product.objects.all()  # услуги
+        leads = Lead.objects.annotate(count=Count("id"))  # потенциальные клиенты
+        customers = Customer.objects.annotate(count=Count("id"))  # активные клиенты
 
-    ads = Ad.objects.annotate(
-        count_leads=Count('lead'),
-        company_success=ExpressionWrapper(
-            (Sum('product__contract__cost') - F('advertising_budget')),
-            output_field=DecimalField()
+        ads = Ad.objects.annotate(
+            count_leads=Count('lead'),
+            company_success=ExpressionWrapper(
+                (Sum('product__contract__cost') - F('advertising_budget')),
+                output_field=DecimalField()
+            )
         )
-    )
 
-    context = {
-        "products": products,
-        "ads": ads,
-        "leads": leads,
-        "customers": customers,
-    }
-    return render(request, "statisticsapp/index.html", context=context)
+        context = {
+            "products": products,
+            "ads": ads,
+            "leads": leads,
+            "customers": customers,
+        }
+        return render(request, "statisticsapp/index.html", context=context)
+    else:
+        return redirect("myauth:login")
